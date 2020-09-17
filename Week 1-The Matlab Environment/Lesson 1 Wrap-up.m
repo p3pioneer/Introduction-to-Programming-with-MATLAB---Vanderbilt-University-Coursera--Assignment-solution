9a7eb507de55b8dbd5a02c2a6675eeb79bba31f6
Pujan Patel <65822379+p3pioneer@users.noreply.github.com>
2020-09-17 23:44:32 +0530

    Lesson 1 Wrap-up

---
+++ Week 1-The Matlab Environment/Lesson 1 Wrap-up.m
@@ -0,0 +1,11 @@
+Que:1.As of early 2018,usain bolt hold the world record int he men 100 meter race.It is 9.58 seconds.What was his avg speed in km/h??Assign result to variable called "hundred".
+    2.Kenyan Eulid set a new world record for a men of 2:01:39 on sept 2018.Assign his avg speed in km/h to variable "marathon".the marathon distance is 42.195 kilometers.
+
+
+
+Answer:
+
+sis=9.58
+hundred=3600*0.1/sis
+hour=2+1/60+39/3600
+marathon=42.195/hour
