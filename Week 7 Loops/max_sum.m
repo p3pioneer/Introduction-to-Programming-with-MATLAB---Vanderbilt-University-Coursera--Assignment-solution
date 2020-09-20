e0a9c5558b500563339c5c48239efc69018f42bb
Pujan Patel <65822379+p3pioneer@users.noreply.github.com>
2020-09-20 21:24:45 +0530

    lesson 6 wrap up

---
+++ Week 7 Loops/max_sum.m
@@ -0,0 +1,66 @@
+%%my code
+function [summa,index]=max_sum(v,n)
+summa=0;
+m=length(v);
+if nargin<2||n==m;
+    index=1;
+    summa=sum(v);
+else    
+
+  if m<n
+    summa=0;
+    index=-1;
+   else
+   
+    
+   for i=1:m-n
+       p=i;
+       psum=0;
+        for k=1:n
+           psum=psum+v(p);
+           p=p+1;
+        end
+      
+       partialsum(i)=psum
+ 
+   end
+ [summa,index]=max(partialsum);
+  end
+end
+end
+ 
+%%function [summa, ind] = max_sum(v,n)
+    % If n is greater than v return the specified values
+    % Using return keyword exits the function so no further code is
+    % evaluated
+   
+    %if n > length(v)
+     %   summa = 0;
+      %  ind = -1;
+       % return;
+   % end
+    
+    % Initialize summa to -inf.
+    % Then work through the vector, checking if each sum is larger than the
+    % current value of summa
+    
+    %summa = -inf;
+    %ind = -1;
+    % Once we get to length(v)-n+1 we stop moving through the vector
+   % for ii = 1:length(v)-n+1
+    %    currentV = v(ii:(ii+n-1));
+     %   currentSumma = sum(currentV);
+        % If currentSumma greater than summa, update summa and ind
+      %  if currentSumma > summa
+       %     summa = currentSumma;
+        %    ind = ii;
+        %end
+    %end
+    %end
+
+           
+       
+    
+
+
+
